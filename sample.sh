b8ccf154f92d9d2def4acb91d3e21112911ec31d
blessedralph <94093325+blessedralph@users.noreply.github.com>
2022-12-18 17:48:16 -0500

    3rd updated sample.sh

--- sample.sh
+++ sample.sh
@@ -3,5 +3,5 @@ echo "I am learning to merge/conflict code with git on AWS, its been fun"
 
 echo "Trying to do more practice"
 
-echo "Trying to do more fantastic practice"
+echo "Trying to do more fantastic practice/job"
 

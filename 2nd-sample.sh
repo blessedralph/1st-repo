03866f615f0696c371e1b329d953868ed985b56d
blessedralph <94093325+blessedralph@users.noreply.github.com>
2022-12-30 14:24:46 -0500

    Update 2nd-sample.sh

--- 2nd-sample.sh
+++ 2nd-sample.sh
@@ -9,3 +9,5 @@ echo "it's because god is involve and na him dey run am"
 echo "we're still on the git mata and I'm improving by the day"
 
 echo "it gets easier with consistent practicing"
+
+echo "because na God dey run am"

d7a53d76d12de68474296e00c7c8bb777c9753e8
blessedralph <94093325+blessedralph@users.noreply.github.com>
2022-12-29 20:59:42 -0500

    Update 2nd-sample.sh

--- 2nd-sample.sh
+++ 2nd-sample.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
 echo "We are still moving by His grace, HE is our strenght"
 
 echo "we keep grinding and we keep winning"
+
 echo "it's because god is involve and na him dey run am"
 
+echo "we're still on the git mata"
 
